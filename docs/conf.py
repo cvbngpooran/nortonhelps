44433091c2eb9174ccfcee6f741a31071da870c5
cvbngpooran <cvbngpooran@outlook.com>
2025-06-20 15:38:19 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'alabaster'
-html_static_path = ['_static']    //conf.py
+html_static_path = ['_static']  
 
 html_js_files = [
     'livechat.js',
